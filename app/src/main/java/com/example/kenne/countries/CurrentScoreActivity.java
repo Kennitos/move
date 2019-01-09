7b0f122f9dcbc56bd382a144e92bf9c61c13a9fb
Kennet <you@example.com>
2019-01-09 16:05:16 +0100

    add Cscore

---
+++ app/src/main/java/com/example/kenne/countries/CurrentScoreActivity.java
@@ -0,0 +1,13 @@
+package com.example.kenne.countries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_score);
+    }
+}
--- app/src/main/java/com/example/kenne/countries/QuizActivity.java
+++ app/src/main/java/com/example/kenne/countries/QuizActivity.java
@@ -1,7 +1,9 @@
 package com.example.kenne.countries;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    public void goToCurrentScoreActivity(View view){
+        Intent intent = new Intent(this, CurrentScoreActivity.class);
+        startActivity(intent);
     }
 }
